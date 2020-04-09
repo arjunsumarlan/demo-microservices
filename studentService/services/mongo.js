de96b421e8473493fc090796e70178defabdee1c
macbook-pro15 <arjunsumarlan@gmail.com>
2020-04-09 11:04:02 +0700

    init

--- studentService/services/mongo.js
+++ studentService/services/mongo.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient
 const url = 'mongodb://localhost:27017'
-const dbName = 'majestic-fox'
+const dbName = 'native-fox'
 const client = new MongoClient(url)
 
 module.exports = client
